dd61d3b1f7c279250fe4fe06a3a58ba31867ea84
sretenovic13 <117783260+sretenovic13@users.noreply.github.com>
2024-02-19 17:55:16 +0100

    person

---
+++ person.js
@@ -0,0 +1,13 @@
+export default class Person {
+    constructor(first, last, education, age, eye) {
+        this.firstName = first
+        this.lastName = last
+        this.education = education
+        this.age = age
+        this.eye = eye
+    }
+    fullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
